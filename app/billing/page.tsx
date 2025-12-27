6a44452e23ff535aa52d5dee92d37805c871f7f8
myusein01 <myusein01@gmail.com>
2025-12-27 13:36:44 +0100

    Sixth attempt

--- app/billing/page.tsx
+++ app/billing/page.tsx
@@ -1,6 +1,8 @@
 // app/billing/page.tsx
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
